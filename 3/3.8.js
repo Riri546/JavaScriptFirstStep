5fadedec2d5d197908d21201bef9b2847ef199cf
Лилия <l_loshakova123@mail.ru>
2023-01-31 00:39:55 +0300

    Sketch

---
+++ 3/3.8.js
@@ -0,0 +1,30 @@
+
+<><head>
+    <script type="text/javascript" src="3.8.js"></script>
+</head><body>
+        <h1>Длина окружностей</h1>
+        <table>
+            <script type="text/javascript">
+                'use strict';
+                /*
+                Строгий режим включен!
+                Не забывай объявлять все переменные!
+                */
+
+                function circleLength(d) { d * Math.PI }
+                
+                
+
+                window.document.write('<tr><td>2</td>');
+                    window.document.write('<td>', circleLength(2), '</td></tr>');
+                window.document.write('<tr><td>5</td>');
+                    window.document.write('<td>', circleLength(5), '</td></tr>');
+                window.document.write('<tr><td>10</td>');
+                    window.document.write('<td>', circleLength(10), '</td></tr>');
+                window.document.write('<tr><td>20</td>');
+                    window.document.write('<td>', circleLength(20), '</td></tr>');
+
+            </script>
+        </table>
+    </body></>
+
