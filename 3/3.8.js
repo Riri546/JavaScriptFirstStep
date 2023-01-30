da2ce99b40c2272da7584611f62a4166ed2620f2
Лилия <l_loshakova123@mail.ru>
2023-01-31 00:54:28 +0300

    Sketch

--- 3/3.8.js
+++ 3/3.8.js
@@ -1,30 +1,30 @@
-
 <><head>
-    <script type="text/javascript" src="3.8.js"></script>
-</head><body>
-        <h1>Длина окружностей</h1>
-        <table>
-            <script type="text/javascript">
-                'use strict';
-                /*
-                Строгий режим включен!
-                Не забывай объявлять все переменные!
-                */
+    {/* <script type="text/javascript" src="3.8.js"></script> */}
+    <script type="text/javascript" charset="utf-8" src="3.8.js"></script>
+</head>
+<body>
+    <h1>Длина окружностей</h1><table>
+    <script>
+        'use strict';
+        /*
+        Строгий режим включен!
+        Не забывай объявлять все переменные!
+        */
 
-                function circleLength(d) { d * Math.PI }
-                
-                
+        function circleLength(d) {d * Math.PI}
 
-                window.document.write('<tr><td>2</td>');
-                    window.document.write('<td>', circleLength(2), '</td></tr>');
-                window.document.write('<tr><td>5</td>');
-                    window.document.write('<td>', circleLength(5), '</td></tr>');
-                window.document.write('<tr><td>10</td>');
-                    window.document.write('<td>', circleLength(10), '</td></tr>');
-                window.document.write('<tr><td>20</td>');
-                    window.document.write('<td>', circleLength(20), '</td></tr>');
+        window.document.write('<tr><td>2</td>');
+            window.document.write('<td>', circleLength(2), '</td></tr>');
+        window.document.write('<tr><td>5</td>');
+            window.document.write('<td>', circleLength(5), '</td></tr>');
+        window.document.write('<tr><td>10</td>');
+            window.document.write('<td>', circleLength(10), '</td></tr>');
+        window.document.write('<tr><td>20</td>');
+            window.document.write('<td>', circleLength(20), '</td></tr>');
 
-            </script>
-        </table>
+    </script>
+</table>
     </body></>
+
+
 
