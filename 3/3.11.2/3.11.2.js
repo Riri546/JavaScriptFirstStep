d14153b77c510bd3157c6c1de928f5895ec83bba
Лилия <l_loshakova123@mail.ru>
2023-01-31 03:21:40 +0300

    Sketch

--- 3/3.11.2/3.11.2.js
+++ 3/3.11.2/3.11.2.js
@@ -1,6 +1,9 @@
 function average() {
     let l = arguments.length, i;
-    for (i = 0; i < l; i++) {
-        window.document.write( arguments[i] + ' ');
+    if (arguments === Number){
+        for (i = 0; i < l; i++) {
+            window.document.write( arguments[i] + ' ');
+        }
     }
+    
 }
