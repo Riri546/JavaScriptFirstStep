de4dd7a896171fe3db057d7781f3032b2d5c7f13
Лилия <l_loshakova123@mail.ru>
2023-01-31 04:19:13 +0300

    Sketch

--- 3/3.11.2/3.11.2.js
+++ 3/3.11.2/3.11.2.js
@@ -1,16 +1,15 @@
 //Все работает, но нужно сделать апгрейд в виде проверки на строки, а то вторая часть тз не работает 
 function average() {
     let i, el, l = arguments.length;
-    let sum = 0;
-        for (i = 0; i < l; i++) {
-            el = arguments[i]
+    let sum = 0, count = 0;
+    for (i = 0; i < l; i++) {
+        el = arguments[i]
+        if (typeof el == 'number') {
             window.document.writeln(el)
-            if (typeof el == 'number') {
-                sum += el;
-            }
+            sum += el;
+            count++
         }
-        return sum / l;
-        
+    }
+    return sum / count;
 }
 
-        
