58c1c0adcbc015a6e94e241a68569b719855f1f7
Лилия <l_loshakova123@mail.ru>
2023-01-31 03:43:14 +0300

    Sketch

--- 3/3.11.2/3.11.2.js
+++ 3/3.11.2/3.11.2.js
@@ -1,12 +1,12 @@
-function average() {
-    let l = arguments.length, i, sum;
-    // if (arguments == Number) {
-        for (i = 0; i < l; i++) {
-            window.document.write(arguments[i] + ' ');
-            sum += arguments[i];
-            window.document.write(arguments[i] + ': ' + sum / l);
-        }  
-    // }
-}
-average(10, 20, 30, 40, 50, 60, 70, 80, 90, 100)
-average(10, 20, 30, '40', 50, '60', 70, '80', 90, '100');
+// function average() {
+//     let l = arguments.length, i, sum;
+//     // if (arguments == Number) {
+//         for (i = 0; i < l; i++) {
+//             window.document.write(arguments[i] + ' ');
+//             sum += arguments[i];
+//             window.document.write(arguments[i] + ': ' + sum / l);
+//         }  
+//     // }
+// }
+// average(10, 20, 30, 40, 50, 60, 70, 80, 90, 100)
+// average(10, 20, 30, '40', 50, '60', 70, '80', 90, '100');
