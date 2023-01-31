a20b1e23a456417982e93f423f98d8d21ee3cd65
Лилия <l_loshakova123@mail.ru>
2023-01-31 04:13:52 +0300

    Sketch

--- 3/3.11.2/3.11.2.js
+++ 3/3.11.2/3.11.2.js
@@ -3,11 +3,13 @@ function average() {
     let i, avg, l = arguments.length;
     let sum = 0;
         for (i = 0; i < l; i++) {
-            sum = sum + arguments[i];
             window.document.writeln(arguments[i])
+            if (typeof el == 'number') {
+                sum = sum + arguments[i];
+            }
         }
-        avg = sum / l;
-        window.document.write('<p>', 'Длина = ' + l, '</p>')
-        window.document.writeln('<p>', 'Сумма = ' + sum, '</p>')
-        window.document.writeln('<p>', "Среднее арифметическое = " + avg, '</p>')
+        return avg = sum / l;
+        
 }
+
+        
