82e559d1075d3fd3a1e6d60f92bc3c92ef9e9373
Лилия <l_loshakova123@mail.ru>
2023-02-01 01:19:01 +0300

    Sketch

--- 3/3.11.3/3.11.3.js
+++ 3/3.11.3/3.11.3.js
@@ -6,7 +6,7 @@ function average2(elmt,) {
         for (j = 0; j < leng; j++) {
             el1 = arguments[i]
             el2 = elmt[i]
-            if (typeof el1 == 'number' && typeof el2 == 'number') {
+            if (typeof el1 == 'number' && typeof el2 == 'array') {
                 window.document.writeln(el1, el2)
                 sum += el1;
                 sum += el2;
@@ -16,4 +16,25 @@ function average2(elmt,) {
     }
     return sum / count;
 }
-
+function average2() {
+    const cntAll = arguments.length;
+    let sum = 0, cnt = 0, i, el, j, el2;
+    for (i = 0; i < cntAll; i++) {
+        el = arguments[i];
+        switch(typeof el) {
+            case 'number':
+                sum += el;
+                cnt++;
+                break;
+            case 'object':
+                for (j = 0; j < el.length; j++) {
+                    el2 = el[j];
+                    if (typeof el2 == 'number') {
+                        sum += el2;
+                        cnt++;
+                    }
+                }
+        }
+    }
+    return sum / cnt;
+}
