68ac7856b4903f29db8f9a4da95ba84f1970bcc4
Лилия <l_loshakova123@mail.ru>
2023-01-31 04:19:40 +0300

    Sketch

--- 3/3.11.2/3.11.2.js
+++ 3/3.11.2/3.11.2.js
@@ -1,4 +1,3 @@
-//Все работает, но нужно сделать апгрейд в виде проверки на строки, а то вторая часть тз не работает 
 function average() {
     let i, el, l = arguments.length;
     let sum = 0, count = 0;
