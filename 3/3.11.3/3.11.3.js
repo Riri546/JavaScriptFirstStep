e2890f1ba26391b4bbc564f07a2c0368e79bff42
Лилия <l_loshakova123@mail.ru>
2023-01-31 04:21:53 +0300

    New file

---
+++ 3/3.11.3/3.11.3.js
@@ -0,0 +1,14 @@
+function average() {
+    let i, el, l = arguments.length;
+    let sum = 0, count = 0;
+    for (i = 0; i < l; i++) {
+        el = arguments[i]
+        if (typeof el == 'number') {
+            window.document.writeln(el)
+            sum += el;
+            count++
+        }
+    }
+    return sum / count;
+}
+
