06e2f7eb17d7d44a71e9ee57767cda4b68e8d491
Лилия <l_loshakova123@mail.ru>
2023-02-01 01:30:06 +0300

    Sketch

--- 4/4.14.1/4.14.1.js
+++ 4/4.14.1/4.14.1.js
@@ -1,6 +1,7 @@
 function average2() {
     let i, el, l = arguments.length;
     let sum = 0, count = 0;
+    
     for (i = 0; i < l; i++) {
         el = arguments[i]
         if (typeof el == 'number') {
