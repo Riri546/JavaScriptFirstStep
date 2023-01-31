a1e22fe7e51d17fb82f5af6b29a0d2034fd8716d
Лилия <l_loshakova123@mail.ru>
2023-02-01 01:14:46 +0300

    Rename

--- 3/3.11.3/3.11.3.js
+++ 3/3.11.3/3.11.3.js
@@ -1,12 +1,17 @@
-function average2() {
-    let i, el, l = arguments.length;
+function average2(elmt,) {
+    var elmt = new Array();
+    let i, j, el1, el2, l = arguments.length, leng = elmt.length;
     let sum = 0, count = 0;
     for (i = 0; i < l; i++) {
-        el = arguments[i]
-        if (typeof el == 'number') {
-            window.document.writeln(el)
-            sum += el;
-            count++
+        for (j = 0; j < leng; j++) {
+            el1 = arguments[i]
+            el2 = elmt[i]
+            if (typeof el1 == 'number' && typeof el2 == 'number') {
+                window.document.writeln(el1, el2)
+                sum += el1;
+                sum += el2;
+                count++
+            }
         }
     }
     return sum / count;
