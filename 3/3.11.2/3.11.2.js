3debade48d0164ff51c5245ec4991f282d158f5d
Лилия <l_loshakova123@mail.ru>
2023-01-31 03:18:33 +0300

    Sketch

--- 3/3.11.2/3.11.2.js
+++ 3/3.11.2/3.11.2.js
@@ -1,8 +1,6 @@
 function average() {
     let l = arguments.length, i;
-    window.document.write('<ul>');
-    for(i = 0; i < l; i++){
-        window.document.write("<li>", arguments[i], "/li");
-    window.document.write("</ul>");
+    for (i = 0; i < l; i++) {
+        window.document.write( arguments[i] + ' ');
     }
 }
